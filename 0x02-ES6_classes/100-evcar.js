360460c981b5025f728a02b343837c8d7ea48a2f
Magret <oyedeleabiodun79@gmail.com>
2024-05-31 16:02:59 -0230

    Advanced task solution

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,40 @@
+// 100-evcar.js
+
+export class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
