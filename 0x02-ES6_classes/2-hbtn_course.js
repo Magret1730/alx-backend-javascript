924e9d425e65c93619300ca25753d31f119b9707
Magret <oyedeleabiodun79@gmail.com>
2024-05-31 12:17:30 -0230

    Solutions to task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,57 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    // Verify types
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array');
+    }
+    // Set attributes
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(students) {
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = students;
+  }
+}
