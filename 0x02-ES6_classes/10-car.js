ea64d1a9ba3affe5f9e4591a68ee8419791c8935
Magret <oyedeleabiodun79@gmail.com>
2024-05-31 15:38:51 -0230

    Task 10 solution

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,32 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string') {
+      return new TypeError('Brand must be a string');
+    }
+    if (typeof motor !== 'string') {
+      return new TypeError('Motor must be a string');
+    }
+    if (typeof color !== 'string') {
+      return new TypeError('Color must be a string');
+    }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
