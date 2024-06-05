cd111ba6cc7250315250fe40ff2f651eade0b216
Magret <oyedeleabiodun79@gmail.com>
2024-06-05 15:43:27 -0230

    Task 3 solution

--- 0x04-TypeScript/task_1/dist/js/main.js
+++ 0x04-TypeScript/task_1/dist/js/main.js
@@ -8,7 +8,24 @@ var TeacherImp = /** @class */ (function () {
     }
     return TeacherImp;
 }());
+// Example usage
 var teacher3 = new TeacherImp('John', 'Doe', false, 'London');
 teacher3.contract = false;
 console.log(teacher3);
+// Example usage
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
+// Implement printTeacher function
+function printTeacher(firstName, lastName) {
+    var firstLetter = firstName.charAt(0);
+    return "".concat(firstLetter, ". ").concat(lastName);
+}
+// Example usage
+console.log(printTeacher("John", "Doe"));
 //# sourceMappingURL=main.js.map
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,7 +30,37 @@ class TeacherImp {
     }
 }
 
+// Example usage
 const teacher3: Teacher = new TeacherImp( 'John','Doe', false, 'London')
 teacher3.contract = false;
 
 console.log(teacher3);
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+// Example usage
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+// Define interface for printTeacher function
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+// Implement printTeacher function
+function printTeacher(firstName: string, lastName: string): string {
+    const firstLetter = firstName.charAt(0);
+
+    return `${firstLetter}. ${lastName}`;
+}
+
+// Example usage
+console.log(printTeacher("John", "Doe")); 
